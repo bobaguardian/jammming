ccebc1f1882670977a08d6e0380ffa6c2feff871
Vivian Thach <vivianlthach@gmail.com>
2021-05-12 10:46:47 -0700

    Edit Spotify.js to pass compile

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -55,9 +55,7 @@ const Spotify = {
         const headers = { Authorization: `Bearer ${accessToken}`};
         let userId;
 
-        return fetch('https://api.spotify.com/v1/me', {headers: headers}).
-        then(response => response.json()).
-        then(jsonResponse => {
+        return fetch('https://api.spotify.com/v1/me', {headers: headers}).then(response => response.json()).then(jsonResponse => {
             userId = jsonResponse.id;
             return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`,
             {
